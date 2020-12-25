90ea6061c82fa696228ed12f0f8965237541bdd0
Arnold deVos <adv@bgsig.com>
2020-12-26 09:37:53 +1100

    as built

---
+++ build-led-control.sh
@@ -0,0 +1,4 @@
+sudo apt install git scons swig python3-setuptools python3-pip
+git clone --recurse-submodules https://github.com/jackw01/led-control.git
+cd led-control
+sudo python3 setup.py develop
